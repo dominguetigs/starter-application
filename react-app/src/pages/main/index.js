4649d3ff278b55e45d22023e8ff8f198e27ae0a3
Gustavo Santana Domingueti <gustavo.s.domingueti@gmail.com>
2020-05-30 20:18:21 -0300

    - Configuração de roteamento; - Terminado página de produtos.

--- react-app/src/pages/main/index.js
+++ react-app/src/pages/main/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { Link } from 'react-router-dom';
+
 import './styles.css';
 
 import api from '../../services/api';
@@ -48,7 +50,7 @@ export default class Main extends Component {
   };
 
   render() {
-    const { products } = this.state;
+    const { products, productInfo, page } = this.state;
 
     return (
       <div className="product-list">
@@ -56,12 +58,17 @@ export default class Main extends Component {
           <article key={product._id}>
             <strong>{product.title}</strong>
             <p>{product.description}</p>
-            {/* <a href="#">Acessar</a> */}
+
+            <Link to={`/products/${product._id}`}></Link>
           </article>
         ))}
         <div className="actions">
-          <button onClick={this.prevPage}>Anterior</button>
-          <button onClick={this.nextPage}>Próxima</button>
+          <button disabled={page === 1} onClick={this.prevPage}>
+            Anterior
+          </button>
+          <button disabled={page === productInfo.pages} onClick={this.nextPage}>
+            Próxima
+          </button>
         </div>
       </div>
     );
