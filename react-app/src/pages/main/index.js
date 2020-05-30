ca68c0414c06218624824861db22116eb3bf458f
Gustavo Santana Domingueti <gustavo.s.domingueti@gmail.com>
2020-05-30 18:28:14 -0300

    Criado arquivo main para listar produtos.

---
+++ react-app/src/pages/main/index.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+import './styles.css';
+
+import api from '../../services/api';
+
+export default class Main extends Component {
+  state = {
+    products: [],
+    productInfo: {},
+    page: 1,
+  };
+
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
+
+    const { docs, ...productInfo } = response.data;
+
+    this.setState({ products: docs, productInfo, page });
+  };
+
+  prevPage = () => {
+    const { page } = this.state;
+
+    if (page === 1) {
+      return;
+    }
+
+    const pageNumber = page - 1;
+
+    this.loadProducts(pageNumber);
+  };
+
+  nextPage = () => {
+    const { page, productInfo } = this.state;
+
+    if (page === productInfo.pages) {
+      return;
+    }
+
+    const pageNumber = page + 1;
+
+    this.loadProducts(pageNumber);
+  };
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <div className="product-list">
+        {products.map((product) => (
+          <article key={product._id}>
+            <strong>{product.title}</strong>
+            <p>{product.description}</p>
+            {/* <a href="#">Acessar</a> */}
+          </article>
+        ))}
+        <div className="actions">
+          <button onClick={this.prevPage}>Anterior</button>
+          <button onClick={this.nextPage}>Próxima</button>
+        </div>
+      </div>
+    );
+  }
+}
