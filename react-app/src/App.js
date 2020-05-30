5e1bd8d5ab3f0fa9ed983f7d146f1d0344bb79c7
Gustavo Santana Domingueti <gustavo.s.domingueti@gmail.com>
2020-05-30 16:52:50 -0300

    - Criado componente header; - Estilizaçao do header; - Estilização global.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-function App() {
-  return <h1>Hello World!</h1>;
-}
+import './styles.css';
+
+import Header from './components/Header';
+
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
 
 export default App;
